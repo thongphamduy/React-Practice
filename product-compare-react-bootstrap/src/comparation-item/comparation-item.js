4ea98423dbd731bc0f9b996e2d4534d10a560bef
thong <thong@inspectorio.com>
2018-05-19 23:30:37 +0700

    Revert "Revert "done, will make it responsible later""
    This reverts commit c2fedb17ef786fb0e7286b43c6a8398a0d1f6ed0.

---
+++ product-compare-react-bootstrap/src/comparation-item/comparation-item.js
@@ -0,0 +1,11 @@
+
+class ComparationItem extends React.Component {
+  
+  render() {
+    return (
+      <div>
+        <div></div>
+      </div>
+    );
+  }
+}
--- product-compare-react-bootstrap/src/product-containter/product-container.js
+++ product-compare-react-bootstrap/src/product-containter/product-container.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import Product from '../product/product';
+import product from '../product/product';
 
 class ProductContainer extends React.Component {
   constructor(props) {
     super(props);
+    this.handleClickProduct = this.handleClickProduct.bind(this);
+  }
+
+  handleClickProduct(id) {
+    console.log(id);
+    this.props.onSelectProduct(id);
   }
 
   render() {
     return (
-      <div className="container">
-        <Product/>
-        <Product/>
-        <Product/>
+      <div className="d-flex">
+        {this.props.data.map(item => <Product data={item} key={item.id} 
+          handleClickProduct={this.handleClickProduct}>
+        </Product>)}
       </div>
     );
   }
